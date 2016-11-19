cb959175db6333811f167d9d460d4fed5be44271
Linyang <personal.linyang@gmail.com>
2016-11-19 12:46:44 +0900

    404ページ作成

--- fuel/app/classes/controller/404.php
+++ fuel/app/classes/controller/404.php
@@ -15,6 +15,14 @@ class Controller_404 extends Controller_App
 	public function action_index()
 	{
 		$data = array();
+		
+		//共通ヘッダー取得
+		$data['header'] = Request::forge('common/header')->execute()->response();
+		//共通サイドバー取得
+		$data['sidebar'] = Request::forge('common/sidebar')->execute()->response();
+		//共通フッター取得
+		$data['footer'] = Request::forge('common/footer')->execute()->response();
+		
 		return Response::forge(View::forge($this->template . '/404', $data, false));
 	}
 	
--- fuel/app/views/pc/404.php
+++ fuel/app/views/pc/404.php
@@ -3,8 +3,28 @@
 <head>
 	<meta charset="utf-8">
 	<title>お探しのページは存在しません</title>
+	<?php echo Asset::css('pc/common.css'); ?>
+	<?php echo Asset::css('pc/index.css'); ?>
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
+	<?php echo Asset::js('pc/common.js'); ?>
+	<?php echo Asset::js('pc/index.js'); ?>
 </head>
-<body>
-	PC404
+<body class="body-common">
+	<?php echo $header; ?>
+	<div class="body-area">
+		<div class="main-area">
+			<div class="main-article main-404">
+				<h2>誠に申し訳ございません！</h2>
+				<h2>お探しのページは見つかりませんでした。</h2>
+				<h2>サイト更新などによってURLが変更になったか、</h2>
+				<h2>URLが正しく入力されていない可能性があります。</h2>
+				<h2>お手数ですが下部サイトマップより</h2>
+				<h2>お求めのページをお探しください。</h2>
+				<div class="link_top"><a href="/">トップページへ戻る</a></div>
+			</div>
+		</div>
+		<?php echo $sidebar; ?>
+	</div>
+	<?php echo $footer; ?>
 </body>
 </html>
