ae42b3fcd56a7d750e724d825cd1f7f7d6621057
Linyang <personal.linyang@gmail.com>
2016-12-04 12:20:36 +0900

    404ページ実装とトップページTDK修正

--- fuel/app/views/sp/404.php
+++ fuel/app/views/sp/404.php
@@ -2,9 +2,28 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<title>お探しのページは存在しません</title>
+	<title>お探しのページは見つかりませんでした - タンタンメン本舗</title>
+	<meta name="description" content="お探しのページは見つかりませんでした。タンタンメン本舗は神奈川県横浜市坂東橋近くのタンタンメン専門店です。タンタンメンだけではなく、ラーメン、チャーハン、餃子、単品料理など様々な美味しい料理を販売しています、メニューを見るとお気に入りの料理がきっと見つかります。美味しい料理食べたいならぜひタンタンメン本舗へ！">
+	<meta name="keywords" content="お探しのページは見つかりませんでした,404,Not Found">
+	<link rel="canonical" href="http://tantanmen-honpo.jp/">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+	<?php echo Asset::css('sp/common.css'); ?>
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
+	<?php echo Asset::js('pc/google-analytics.js'); ?>
+	<?php echo Asset::js('sp/common.js'); ?>
 </head>
-<body>
-	SP404
+<body class="body-common">
+	<?php echo $header; ?>
+
+	<div class="main-article main-404">
+		<h2>誠に申し訳ございません！</h2>
+		<h2>お探しのページは<br/>見つかりませんでした。</h2>
+		<h2>サイト更新などによって<br/>URLが変更になったか、</h2>
+		<h2>URLが正しく入力されていない<br/>可能性があります。</h2>
+		<h2>お手数ですがメニューより<br/>お求めのページをお探しください。</h2>
+		<div class="link-top"><a href="/">トップページへ戻る</a></div>
+	</div>
+	
+	<?php echo $footer; ?>
 </body>
 </html>
--- fuel/app/views/sp/index.php
+++ fuel/app/views/sp/index.php
@@ -3,10 +3,18 @@
 <head>
 	<meta charset="utf-8">
 	<title>タンタンメン本舗</title>
+	<meta name="description" content="タンタンメン本舗は神奈川県横浜市坂東橋近くのタンタンメン専門店です。タンタンメンだけではなく、各種麺類、各種ご飯、餃子、単品料理、定食料理、ちょい飲みなど様々な美味しい料理を販売しています、メニューを見るとお気に入りの料理がきっと見つかります。美味しい料理食べたいならぜひタンタンメン本舗へ！">
+	<meta name="keywords" content="タンタンメン本舗,横浜,坂東橋,曙町">
+	<link rel="canonical" href="http://tantanmen-honpo.jp/">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
 	<?php echo Asset::css('sp/common.css'); ?>
 	<?php echo Asset::css('sp/index.css'); ?>
 	<?php echo Asset::css('sp/download/slider-pro.css'); ?>
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
+	<?php echo Asset::js('pc/google-analytics.js'); ?>
+	<?php echo Asset::js('sp/common.js'); ?>
+	<?php echo Asset::js('sp/index.js'); ?>
+	<?php echo Asset::js('sp/download/jquery.sliderPro.js'); ?>
 </head>
 <body class="body-common">
 	<?php echo $header; ?>
@@ -44,9 +52,5 @@
 	</div>
 
 	<?php echo $footer; ?>
-	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
-	<?php echo Asset::js('sp/common.js'); ?>
-	<?php echo Asset::js('sp/index.js'); ?>
-	<?php echo Asset::js('sp/download/jquery.sliderPro.js'); ?>
 </body>
 </html>
