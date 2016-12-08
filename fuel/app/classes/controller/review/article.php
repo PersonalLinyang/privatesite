2256e2d4e462f0c11f664715a59f1249c66da426
Linyang <personal.linyang@gmail.com>
2016-12-08 23:03:00 +0900

    ネット上の声404リダイレクト修正

--- fuel/app/classes/controller/review/article.php
+++ fuel/app/classes/controller/review/article.php
@@ -67,18 +67,24 @@ class Controller_Review_Article extends Controller_App
 
 		//文章情報取得
 		$article = Model_Article::GetArticle($article_id, 1);
-
-		//TDK
-		$data['title'] = $article->title . ' - タンタンメン本舗';
-		$data['description'] = $article->title . '。タンタンメン本舗のレビュー詳細ページです。タンタンメン本舗は神奈川県横浜市坂東橋近くのタンタンメン専門店です。';
-		$data['keywords'] = 'タンタンメン本舗,レビュー,' . $article->title;
-		$data['canonical'] = 'http://' . $_SERVER['HTTP_HOST'] . '/review/article/detail/' . $article_id . '/';
 		
 		//View呼び出す
 		if($article) {
+			//TDK
+			$data['title'] = $article->title . ' - タンタンメン本舗';
+			$data['description'] = $article->title . '。タンタンメン本舗のレビュー詳細ページです。タンタンメン本舗は神奈川県横浜市坂東橋近くのタンタンメン専門店です。';
+			$data['keywords'] = 'タンタンメン本舗,レビュー,' . $article->title;
+			$data['canonical'] = 'http://' . $_SERVER['HTTP_HOST'] . '/review/article/detail/' . $article_id . '/';
+
 			$data['article'] = $article;
 			return Response::forge(View::forge($this->template . '/review/detail', $data, false));
 		} else {
+			//TDK
+			$data['title'] = 'お探しのページは見つかりませんでした - タンタンメン本舗';
+			$data['description'] = 'お探しのページは見つかりませんでした。タンタンメン本舗は神奈川県横浜市坂東橋近くのタンタンメン専門店です。タンタンメンだけではなく、ラーメン、チャーハン、餃子、単品料理など様々な美味しい料理を販売しています、メニューを見るとお気に入りの料理がきっと見つかります。美味しい料理食べたいならぜひタンタンメン本舗へ！';
+			$data['keywords'] = 'お探しのページは見つかりませんでした,404,Not Found';
+			$data['canonical'] = 'http://' . $_SERVER['HTTP_HOST'] . '/404/';
+			
 			return Response::forge(View::forge($this->template . '/404', $data, false));
 		}
 	}
