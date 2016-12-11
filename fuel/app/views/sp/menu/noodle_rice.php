53ef696f595345ee5e2cc597f0738b2b4ad308c0
Linyang <personal.linyang@gmail.com>
2016-12-11 11:26:32 +0900

    SPサイト麺類ご飯類、当店セットColorBox対応

--- fuel/app/views/sp/menu/noodle_rice.php
+++ fuel/app/views/sp/menu/noodle_rice.php
@@ -7,11 +7,16 @@
 	<meta name="keywords" content="<?php echo $keywords; ?>">
 	<link rel="canonical" href="<?php echo $canonical; ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+	<?php echo Asset::css('sp/download/colorbox.css'); ?>
 	<?php echo Asset::css('sp/common.css'); ?>
 	<?php echo Asset::css('sp/menu.css'); ?>
+	<?php echo Asset::css('sp/menu/noodle-rice.css'); ?>
+	<?php echo Asset::css('sp/colorbox-expand.css'); ?>
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
+	<?php echo Asset::js('sp/download/jquery.colorbox.js'); ?>
 	<?php echo Asset::js('sp/google-analytics.js'); ?>
 	<?php echo Asset::js('sp/common.js'); ?>
+	<?php echo Asset::js('sp/menu.js'); ?>
 </head>
 <body class="body-common">
 	<?php echo $header; ?>
@@ -27,18 +32,45 @@
 			<ul class="list-noodle_rice">
 				<?php foreach ($tantanmen_list as $tantanmen) : ?>
 				<li>
-					<div class="div-img">
-						<?php 
-							try {
-								echo Asset::img('menu/product/img_p' . $tantanmen->id . '.jpg');
-							} catch(Exception $e) {
-								echo Asset::img('system/img_product_nofound.jpg');
-							}
-						?>
-					</div>
-					<div class="div-info">
-						<div class="div-title"><?php echo $tantanmen->name; ?></div>
-						<div class="div-price"><span class="price"><?php echo $tantanmen->price; ?></span>円</div>
+					<div class="product-area">
+						<div class="div-img">
+							<?php 
+								try {
+									echo Asset::img('upload/menu/thumb/img_p' . $tantanmen->id . '.jpg');
+								} catch(Exception $e) {
+									echo Asset::img('system/img_product_nofound.jpg');
+								}
+							?>
+							<?php 
+							if($tantanmen->image_list): 
+								$image_list = explode(',', $tantanmen->image_list);
+								$image_count = count($image_list);
+							?>
+							<a rel="colorbox-<?php echo $tantanmen->id; ?>" href="/assets/img/upload/menu/<?php echo $tantanmen->id; ?>/<?php echo $image_list[0]; ?>" title="<?php echo $tantanmen->name; ?>写真　1/<?php echo $image_count; ?>">
+								<div class="div-img-more">
+								</div>
+							</a>
+								<?php if($image_count > 1) : ?>
+							<div class="colorbox-hide">
+									<?php for($counter = 1; $counter < $image_count; $counter++) : ?>
+								<a rel="colorbox-<?php echo $tantanmen->id; ?>" href="/assets/img/upload/menu/<?php echo $tantanmen->id; ?>/<?php echo $image_list[$counter]; ?>" title="<?php echo $tantanmen->name; ?>写真　<?php echo ($counter + 1); ?>/<?php echo $image_count; ?>"></a>
+									<?php endfor; ?>
+							</div>
+							<div class="multi-photo-mark">
+								写真<br/>多数
+							</div>
+							<?php 
+								endif;
+							endif; 
+							?>
+						</div>
+						<div class="div-info">
+							<div class="div-title"><?php echo $tantanmen->name; ?></div>
+							<?php if($tantanmen->caution) : ?>
+							<div class="div-caution">(<?php echo $tantanmen->caution; ?>)</div>
+							<?php endif; ?>
+							<div class="div-price"><span class="price"><?php echo $tantanmen->price; ?></span>円</div>
+						</div>
 					</div>
 				</li>
 				<?php endforeach; ?>
@@ -64,18 +96,45 @@
 			<ul class="list-noodle_rice">
 				<?php foreach ($noodle_list as $noodle) : ?>
 				<li>
-					<div class="div-img">
-						<?php 
-							try {
-								echo Asset::img('menu/product/img_p' . $noodle->id . '.jpg');
-							} catch(Exception $e) {
-								echo Asset::img('system/img_product_nofound.jpg');
-							}
-						?>
-					</div>
-					<div class="div-info">
-						<div class="div-title"><?php echo $noodle->name; ?></div>
-						<div class="div-price"><span class="price"><?php echo $noodle->price; ?></span>円</div>
+					<div class="product-area">
+						<div class="div-img">
+							<?php 
+								try {
+									echo Asset::img('upload/menu/thumb/img_p' . $noodle->id . '.jpg');
+								} catch(Exception $e) {
+									echo Asset::img('system/img_product_nofound.jpg');
+								}
+							?>
+							<?php 
+							if($noodle->image_list): 
+								$image_list = explode(',', $noodle->image_list);
+								$image_count = count($image_list);
+							?>
+							<a rel="colorbox-<?php echo $noodle->id; ?>" href="/assets/img/upload/menu/<?php echo $noodle->id; ?>/<?php echo $image_list[0]; ?>" title="<?php echo $noodle->name; ?>写真　1/<?php echo $image_count; ?>">
+								<div class="div-img-more">
+								</div>
+							</a>
+								<?php if($image_count > 1) : ?>
+							<div class="colorbox-hide">
+									<?php for($counter = 1; $counter < $image_count; $counter++) : ?>
+								<a rel="colorbox-<?php echo $noodle->id; ?>" href="/assets/img/upload/menu/<?php echo $noodle->id; ?>/<?php echo $image_list[$counter]; ?>" title="<?php echo $noodle->name; ?>写真　<?php echo ($counter + 1); ?>/<?php echo $image_count; ?>"></a>
+									<?php endfor; ?>
+							</div>
+							<div class="multi-photo-mark">
+								写真<br/>多数
+							</div>
+							<?php 
+								endif;
+							endif; 
+							?>
+						</div>
+						<div class="div-info">
+							<div class="div-title"><?php echo $noodle->name; ?></div>
+							<?php if($noodle->caution) : ?>
+							<div class="div-caution">(<?php echo $noodle->caution; ?>)</div>
+							<?php endif; ?>
+							<div class="div-price"><span class="price"><?php echo $noodle->price; ?></span>円</div>
+						</div>
 					</div>
 				</li>
 				<?php endforeach; ?>
@@ -113,18 +172,45 @@
 			<ul class="list-noodle_rice">
 				<?php foreach ($rice_list as $rice) : ?>
 				<li>
-					<div class="div-img">
-						<?php 
-							try {
-								echo Asset::img('menu/product/img_p' . $rice->id . '.jpg');
-							} catch(Exception $e) {
-								echo Asset::img('system/img_product_nofound.jpg');
-							}
-						?>
-					</div>
-					<div class="div-info">
-						<div class="div-title"><?php echo $rice->name; ?></div>
-						<div class="div-price"><span class="price"><?php echo $rice->price; ?></span>円</div>
+					<div class="product-area">
+						<div class="div-img">
+							<?php 
+								try {
+									echo Asset::img('upload/menu/thumb/img_p' . $rice->id . '.jpg');
+								} catch(Exception $e) {
+									echo Asset::img('system/img_product_nofound.jpg');
+								}
+							?>
+							<?php 
+							if($rice->image_list): 
+								$image_list = explode(',', $rice->image_list);
+								$image_count = count($image_list);
+							?>
+							<a rel="colorbox-<?php echo $rice->id; ?>" href="/assets/img/upload/menu/<?php echo $rice->id; ?>/<?php echo $image_list[0]; ?>" title="<?php echo $rice->name; ?>写真　1/<?php echo $image_count; ?>">
+								<div class="div-img-more">
+								</div>
+							</a>
+								<?php if($image_count > 1) : ?>
+							<div class="colorbox-hide">
+									<?php for($counter = 1; $counter < $image_count; $counter++) : ?>
+								<a rel="colorbox-<?php echo $rice->id; ?>" href="/assets/img/upload/menu/<?php echo $rice->id; ?>/<?php echo $image_list[$counter]; ?>" title="<?php echo $rice->name; ?>写真　<?php echo ($counter + 1); ?>/<?php echo $image_count; ?>"></a>
+									<?php endfor; ?>
+							</div>
+							<div class="multi-photo-mark">
+								写真<br/>多数
+							</div>
+							<?php 
+								endif;
+							endif; 
+							?>
+						</div>
+						<div class="div-info">
+							<div class="div-title"><?php echo $rice->name; ?></div>
+							<?php if($rice->caution) : ?>
+							<div class="div-caution">(<?php echo $rice->caution; ?>)</div>
+							<?php endif; ?>
+							<div class="div-price"><span class="price"><?php echo $rice->price; ?></span>円</div>
+						</div>
 					</div>
 				</li>
 				<?php endforeach; ?>
--- fuel/app/views/sp/menu/set.php
+++ fuel/app/views/sp/menu/set.php
@@ -7,11 +7,16 @@
 	<meta name="keywords" content="<?php echo $keywords; ?>">
 	<link rel="canonical" href="<?php echo $canonical; ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+	<?php echo Asset::css('sp/download/colorbox.css'); ?>
 	<?php echo Asset::css('sp/common.css'); ?>
 	<?php echo Asset::css('sp/menu.css'); ?>
+	<?php echo Asset::css('sp/menu/set.css'); ?>
+	<?php echo Asset::css('sp/colorbox-expand.css'); ?>
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
+	<?php echo Asset::js('sp/download/jquery.colorbox.js'); ?>
 	<?php echo Asset::js('sp/google-analytics.js'); ?>
 	<?php echo Asset::js('sp/common.js'); ?>
+	<?php echo Asset::js('sp/menu.js'); ?>
 </head>
 <body class="body-common">
 	<?php echo $header; ?>
@@ -24,29 +29,101 @@
 		<div class="main-article-title"><?php echo $set_group['group_name']; ?></div>
 		<div>
 			<ul class="list-set">
-				<?php foreach($set_group['set_list'] as $set) : ?>
+				<?php 
+				foreach($set_group['set_list'] as $set) : 
+					if($set['main_image']) {
+						$main_image_list = explode(',', $set['main_image']);
+					} else {
+						$main_image_list = array();
+					}
+					if($set['sub_image']) {
+						$sub_image_list = explode(',', $set['sub_image']);
+					} else {
+						$sub_image_list = array();
+					}
+					$main_image_count = count($main_image_list);
+					$sub_image_count = count($sub_image_list);
+					$image_count = count($main_image_list) + count($sub_image_list);
+					$image_counter = 1;
+				?>
 				<li>
-					<div class="div-img">
-						<?php 
-							try {
-								echo Asset::img('menu/product/img_p' . $set['main_id'] . '.jpg');
-							} catch(Exception $e) {
-								echo Asset::img('system/img_product_nofound.jpg');
-							}
-						?>
-						<div class="td-sub-img">
+					<div class="product-area">
+						<div class="div-main-img">
 							<?php 
 								try {
-									echo Asset::img('menu/product/img_p' . $set['sub_id'] . '.jpg');
+									echo Asset::img('upload/menu/thumb/img_p' . $set['main_id'] . '.jpg');
 								} catch(Exception $e) {
 									echo Asset::img('system/img_product_nofound.jpg');
 								}
 							?>
+							<?php if($main_image_count) : ?>
+							<a rel="colorbox-<?php echo $set['id']; ?>" href="/assets/img/upload/menu/<?php echo $set['main_id']; ?>/<?php echo $main_image_list[0]; ?>" title="<?php echo $set_group['group_name']; ?>　<?php echo $set['set_name']; ?>写真　1/<?php echo $image_count; ?>">
+								<div class="div-img-more">
+								</div>
+							</a>
+								<?php if($main_image_count > 1) : ?>
+							<div class="colorbox-hide">
+									<?php for($counter = 1; $counter < $main_image_count; $counter++) : ?>
+								<a rel="colorbox-<?php echo $set['id']; ?>" href="/assets/img/upload/menu/<?php echo $set['main_id']; ?>/<?php echo $main_image_list[$counter]; ?>" title="<?php echo $set_group['group_name']; ?>　<?php echo $set['set_name']; ?>写真　<?php echo ($image_counter + 1); ?>/<?php echo $image_count; ?>"></a>
+									<?php 
+										$image_counter++;
+									endfor; 
+									?>
+							</div>
+							<?php 
+								endif;
+							elseif($sub_image_count) : 
+							?>
+							<a class="colorbox-visual">
+								<div class="div-img-more">
+								</div>
+							</a>
+							<?php endif; ?>
+							<?php if($image_count) : ?>
+							<div class="multi-photo-mark">
+								写真<br/>多数
+							</div>
+							<?php endif; ?>
+							<div class="div-sub-img">
+								<?php 
+									try {
+										echo Asset::img('upload/menu/thumb/img_p' . $set['sub_id'] . '.jpg');
+									} catch(Exception $e) {
+										echo Asset::img('system/img_product_nofound.jpg');
+									}
+								?>
+								<?php if($sub_image_count) : ?>
+								<a rel="colorbox-<?php echo $set['id']; ?>" href="/assets/img/upload/menu/<?php echo $set['sub_id']; ?>/<?php echo $sub_image_list[0]; ?>" title="<?php echo $set_group['group_name']; ?>　<?php echo $set['set_name']; ?>写真　<?php echo ($image_counter + 1); ?>/<?php echo $image_count; ?>">
+									<div class="div-img-more">
+									</div>
+								</a>
+									<?php 
+									$image_counter = $image_counter + 1;
+									if($sub_image_count > 1) : 
+									?>
+								<div class="colorbox-hide">
+										<?php for($counter = 1; $counter < $sub_image_count; $counter++) : ?>
+									<a rel="colorbox-<?php echo $set['id']; ?>" href="/assets/img/upload/menu/<?php echo $set['sub_id']; ?>/<?php echo $sub_image_list[$counter]; ?>" title="<?php echo $set_group['group_name']; ?>　<?php echo $set['set_name']; ?>写真　<?php echo ($image_counter + 1); ?>/<?php echo $image_count; ?>"></a>
+										<?php 
+											$image_counter++;
+										endfor; 
+										?>
+								</div>
+								<?php 
+									endif;
+								elseif($main_image_count) : 
+								?>
+								<a class="colorbox-visual">
+									<div class="div-img-more">
+									</div>
+								</a>
+								<?php endif; ?>
+							</div>
 						</div>
-					</div>
-					<div class="div-info">
-						<div class="div-title"><?php echo str_replace(' ＋ ', '<br/> ＋ ', $set['set_name']); ?></div>
-						<div class="div-price"><span class="price"><?php echo $set['price']; ?></span>円</div>
+						<div class="div-info">
+							<div class="div-title"><?php echo $set['set_name']; ?></div>
+							<div class="div-price"><span class="price"><?php echo $set['price']; ?></span>円</div>
+						</div>
 					</div>
 				</li>
 				<?php endforeach; ?>
